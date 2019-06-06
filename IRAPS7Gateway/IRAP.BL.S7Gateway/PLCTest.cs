52bb65e380e5cf070a3b9f13051a5c47a0f11972
李智颖 <zhiying.li@chinairap.com>
2019-06-06 09:55:43 +0800

    首次提交

---
+++ IRAPS7Gateway/IRAP.BL.S7Gateway/PLCTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRAP.BL.S7Gateway
+{
+    public class Class1
+    {
+    }
+}

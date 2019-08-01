7caf20bd9a0d4d3627ccb411cd98c074cb024a26
李智颖 <zhiying.li@chinairap.com>
2019-08-01 17:35:00 +0800

    IRAP-MESGateway网关配置工具

---
+++ IRAPS7Gateway/Tools/SimensPLCDBAEditor/XtraForm1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SimensPLCDBAEditor
+{
+    partial class XtraForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XtraForm1));
+            this.imageListBoxControl1 = new DevExpress.XtraEditors.ImageListBoxControl();
+            this.imageCollection1 = new DevExpress.Utils.ImageCollection(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.imageListBoxControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCollection1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageListBoxControl1
+            // 
+            this.imageListBoxControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageListBoxControl1.ImageList = this.imageCollection1;
+            this.imageListBoxControl1.Location = new System.Drawing.Point(0, 0);
+            this.imageListBoxControl1.Name = "imageListBoxControl1";
+            this.imageListBoxControl1.Size = new System.Drawing.Size(950, 638);
+            this.imageListBoxControl1.TabIndex = 1;
+            // 
+            // imageCollection1
+            // 
+            this.imageCollection1.ImageStream = ((DevExpress.Utils.ImageCollectionStreamer)(resources.GetObject("imageCollection1.ImageStream")));
+            this.imageCollection1.Images.SetKeyName(0, "windows_Application_Options_32px_556156_easyicon.net.png");
+            // 
+            // XtraForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 638);
+            this.Controls.Add(this.imageListBoxControl1);
+            this.Name = "XtraForm1";
+            this.Text = "XtraForm1";
+            this.Load += new System.EventHandler(this.XtraForm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageListBoxControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCollection1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.ImageListBoxControl imageListBoxControl1;
+        private DevExpress.Utils.ImageCollection imageCollection1;
+    }
+}
